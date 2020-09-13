3e7eb9f68700ba82ddbc4a9ccb3c7499142c65d7
E0407750 <e0407750@u.nus.edu>
2020-09-13 10:51:59 +0800

    Added delete command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Arrays;
 
 public class Duke {
-    private static final Task[] userTasks = new Task[100];
+    private static final ArrayList<Task> userTasks = new ArrayList<>();
     private static final String line = "____________________________________________________________";
     private static int taskCount = 0;
 
-    public static void printGreetMessaage() {
+    public static void printGreetMessage() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -25,22 +26,14 @@ public class Duke {
         System.out.println(line);
     }
 
-    public static void printAcknowledgeMessage(Task task) {
-        System.out.println(line);
-        System.out.println("Got it. I've added this task: ");
-        System.out.println("\t[" + task.getType() + "]" + "[✗]" + task.getTaskName());
-        System.out.println("Now you have " + taskCount + " tasks in the list.");
-        System.out.println(line);
-    }
-
     public static void addTask(Task t) {
-        userTasks[taskCount] = t;
+        userTasks.add(t);
         taskCount += 1;
     }
 
-    public static void printMarkDone(int taskId, Task[] userTasks) {
+    public static void printMarkDone(int taskId) {
         System.out.println(line);
-        System.out.println("\tNice! I've marked this task as done: \n\t" + userTasks[taskId].toString());
+        System.out.println("\tNice! I've marked this task as done: \n\t" + userTasks.get(taskId).toString());
         System.out.println(line);
     }
 
@@ -55,8 +48,10 @@ public class Duke {
         System.out.println("B. LIST THE TASK");
         System.out.println("\t list");
         System.out.println("C. MARK THE TASK AS DONE");
-        System.out.println("\t done <task index>");
-        System.out.println("D. END THE PROGRAM");
+        System.out.println("\t done <task number>");
+        System.out.println("D. DELETE THE TASK");
+        System.out.println("\t delete <task number>");
+        System.out.println("E. END THE PROGRAM");
         System.out.println("\t bye");
         System.out.println(line);
     }
@@ -79,14 +74,14 @@ public class Duke {
         System.out.println(line);
     }
 
-    public static void executeListCommand(int taskCount, Task[] userTasks) {
+    public static void executeListCommand() {
         System.out.println(line);
         System.out.println("Here are the tasks in your list:");
-        if (taskCount == 0) {
+        if (userTasks.size() == 0) {
             System.out.println("\tThe task is empty. Please add a task");
         } else {
-            for (int i = 0; i < taskCount; i++) {
-                System.out.println("\t" + (i + 1) + ". " + userTasks[i].toString());
+            for (int i = 0; i < userTasks.size(); i++) {
+                System.out.println("\t" + (i + 1) + ". " + userTasks.get(i).toString());
             }
         }
         System.out.println(line);
@@ -97,44 +92,43 @@ public class Duke {
         //prints the bye message and exits the program
         printByeMessage();
         isFinished = true;
-        return;
     }
 
     private static void executeDeadlineCommand(String parsedMessage) {
         String deadlineName = parsedMessage.substring(0, parsedMessage.indexOf("/") - 1);
         String deadlineBy = parsedMessage.substring(parsedMessage.indexOf("/") + 4);
         addTask(new Deadline(deadlineName, deadlineBy));
-        userTasks[taskCount - 1].printAcknowledgeMessage();
+        userTasks.get(taskCount - 1).printAcknowledgeMessage();
     }
 
     private static void executeEventCommand(String parsedMessage) {
         String eventName = parsedMessage.substring(0, parsedMessage.indexOf("/") - 1);
         String eventTime = parsedMessage.substring(parsedMessage.indexOf("/") + 4);
         addTask(new Event(eventName, eventTime));
-        userTasks[taskCount - 1].printAcknowledgeMessage();
+        userTasks.get(taskCount - 1).printAcknowledgeMessage();
     }
 
     private static void executeToDoCommand(String parsedMessage) {
-        String todoName = parsedMessage;
-        addTask(new Todo(todoName));
-        userTasks[taskCount - 1].printAcknowledgeMessage();
+        addTask(new Todo(parsedMessage));
+        userTasks.get(taskCount - 1).printAcknowledgeMessage();
+    }
+
+    private static void executeDeleteCommand (String parsedMessage){
+        int taskNumber = Integer.parseInt(parsedMessage);
+        userTasks.remove(taskNumber -1);
     }
 
     private static void executeDoneCommand(String parsedMessage) {
-        try{
-            int taskNumber = Integer.parseInt(parsedMessage) - 1;
-            if (taskCount == 0) {
-                printEmptyListNumber();
-            } else if (taskNumber > taskCount) {
-                printExceedTaskNumber();
-            } else if (taskNumber < 0) {
-                printNegativeTaskNumber();
-            } else {
-                userTasks[taskNumber].setDone();
-                printMarkDone(taskNumber, userTasks);
-            }
-        }catch (IndexOutOfBoundsException e){
-            printHelpMessage();
+        int taskNumber = Integer.parseInt(parsedMessage) - 1;
+        if (taskCount == 0) {
+            printEmptyListNumber();
+        } else if (taskNumber > taskCount) {
+            printExceedTaskNumber();
+        } else if (taskNumber < 0) {
+            printNegativeTaskNumber();
+        } else {
+            userTasks.get(taskNumber).setDone();
+            printMarkDone(taskNumber);
         }
     }
 
@@ -142,8 +136,8 @@ public class Duke {
         Scanner myScanner = new Scanner(System.in);
         boolean isFinished = false;
         //greets the user with the greeting message
-        printGreetMessaage();
-        while (!isFinished) {
+        printGreetMessage();
+        while (true) {
             //gets the input from the command line
             String inputMessage = myScanner.nextLine();
             String[] parsedMessages = inputMessage.split(" ", 2);
@@ -152,7 +146,11 @@ public class Duke {
                 executeByeCommand();
                 break;
             } else if (inputMessage.equals("list")) {
-                executeListCommand(taskCount, userTasks);
+                try{
+                    executeListCommand();
+                }catch (IndexOutOfBoundsException e){
+                    printEmptyListNumber();
+                }
             } else if (inputMessage.contains("deadline")) {
                 try {
                     executeDeadlineCommand(parsedMessages[1]);
@@ -177,7 +175,14 @@ public class Duke {
                 }catch (IndexOutOfBoundsException e){
                     printHelpMessage();
                 }
-            } else {
+            } else if (inputMessage.contains("delete")){
+                try {
+                    executeDeleteCommand(parsedMessages[1]);
+                    executeListCommand();
+                }catch (IndexOutOfBoundsException e){
+                    printHelpMessage();
+                }
+            }else {
                 printHelpMessage();
             }
         }
